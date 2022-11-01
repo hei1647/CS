2c0ce50f2d4d9208b02fc3452eb2226f3c4e784c
hei1647 <114565761+hei1647@users.noreply.github.com>
2022-11-01 05:32:47 -0500

    Add files via upload

---
+++ Nested_Conditional_Statement.js
@@ -0,0 +1,52 @@
+alert("While getting ready to leave to place the jewl " +
+"atop the mountain our protaganist has noticed the jewl is " +
+"missing, and is located somewhere in the cabin.");
+
+let Answer1 = prompt("Our protaganist believes it's in the bedroom down " +
+"the dark hallway or by the fireplace. Select where to check first. (1) " +
+"The Bedroom. (2) The Fireplace."
+);
+
+if (Answer1 == "1") {
+alert("While finding his way through the hall he noticed a dark figure " +
+"fly across the bathroom mirrors.");
+let Answer2 = prompt("The protaganist pauses questioning if he should " +
+"turn to look for this figure or continue looking for the jewl in the " +
+"bedroom. (1) Turn around for the dark figure. (2) Continue down the hall. ")
+
+if (Answer2 == "1") {
+alert("As the protaganist leaves the hall he notices a black smoke appears "+
+ "leaving a black dust spread across the carpet. The protaganist decides to "+
+ "go back and check the bedroom"
+ )
+}
+
+else {
+    alert("The protaganist has now made his way to the bedroom")
+}
+
+let Answer3 = prompt("Beeerrrrreww cssskk. the lights have gone out "+
+"and a faint blue light bleeds through the bedroom and hall curtains. (1) "+
+"Check the blue light (2) Search for the jewl")
+
+if (Answer3 == "2") {
+    alert("As the protaganist lays his hands on the shining jewl it "+
+    "begins to shake uncontrollably trying to peel away. PEOW! the jewl explodes "+
+    "into 3 seperate pieces and the blue light from the windows "+
+    "dissapears. Our protaganist can finally continue on his journey "+
+    "but what power does the jewl hold and will it cause our protaganist to become corrupt with evil?")
+}
+else {
+    alert("Our protaganist goes to check" +
+    "out the shining blue light, and is caught into a hypnotizing daze at "+
+    "the light")
+}
+}
+else {
+alert("Beeerrrrreww cssskk. On the way to the fireplace the lights " +
+"have gone out .... a blue light beams down from out the window, barely " +
+"noticable through the thick forest. Our protaganist goes out to check " +
+"out the shining blue light, and is caught into a hypnotizing daze at "+
+"the light ");
+
+}
